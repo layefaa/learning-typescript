1ce8cfaf0e331d5c056237672cb1dfb96fff8c4c
Martin-Luther Obahor <kingm4159@gmail.com>
2024-04-29 02:16:58 +0100

    Data types

---
+++ src/01-Fundamentals.ts
@@ -0,0 +1,19 @@
+// Data Types
+let sales: number = 100_000_000
+let person: string = "james"
+let is_published = false
+
+// any type
+let level: any;
+// any type
+
+// Array
+let arr: number[] = [1, 2, 3]
+let newArr = [1, 2, '3']
+let newArr2: any[] = []
+// Array
+
+// Next ---> Tuples
+
+
+// Data Types
